4ccdaab9d821d6ac3d04dd8a6392d41fc7c8284d
Julia Astapenko <julia.chvyrova@gmail.com>
2024-04-26 22:13:04 +0300

    Fix active

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -39,10 +39,16 @@ const Sidebar: React.FC = () => {
                                 <TooltipTrigger asChild>
                                     <NavLink
                                         to={item.link}
-                                        className={`${({ isActive }) =>
-                                            isActive ? "active" : ""}
-                                            flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8`
-                                        }
+                                        isActive={true}
+                                        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+
+                                        // className={({ isActive }: {isActive: any}) => isActive ? "bg-zinc-950" : "flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"}
+                                        // className={({ isActive, isPending, isTransitioning }) =>
+                                        //     [
+                                        //         "flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8",
+                                        //         isActive ? "bg-zinc-950" : "",
+                                        //     ].join(" ")
+                                        // }
                                     >
                                         {item.icon}
                                         <span className="sr-only">{item.text}</span>
