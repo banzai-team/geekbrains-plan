e65bc37d5d8fdee872d55c8a39c08d459ec7a0d7
chOlya <Chvyrova.o@mail.ru>
2024-04-27 00:49:29 +0300

    prepare shadcn/ui

--- frontend-extension/src/App.tsx
+++ frontend-extension/src/App.tsx
@@ -1,18 +1,21 @@
 import './App.css'
+import {Button} from "~/components/ui/button";
 
 let url = "";
 
 // @ts-ignore
 chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs: any) => {
   url = tabs[0].url;
+  console.log(url) // fix for npm run build
   // use `url` here inside the callback because it's asynchronous!
 });
 
 function App() {
-
   return (
     <>
-      Hello
+      <Button size="icon" variant="outline" className="sm:hidden">
+        Like
+      </Button>
     </>
   )
 }
--- frontend-extension/vite.config.ts
+++ frontend-extension/vite.config.ts
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), crx({ manifest })]
+  plugins: [react(), tsconfigPaths(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
 })
