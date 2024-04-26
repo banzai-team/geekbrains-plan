963ddaad17037e67679840d37689c83c3a7b7ade
chOlya <Chvyrova.o@mail.ru>
2024-04-27 02:41:16 +0300

    show alert on extension button click

--- frontend-extension/src/App.tsx
+++ frontend-extension/src/App.tsx
@@ -4,20 +4,23 @@ import {Button} from "~/components/ui/button";
 let url = "";
 
 // @ts-ignore
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs: any) => {
-  url = tabs[0].url;
-  console.log(url) // fix for npm run build
-  // use `url` here inside the callback because it's asynchronous!
-});
+const onClick = ()=> {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs: any) => {
+        url = tabs[0].url;
+        console.log(url) // fix for npm run build
+        // use `url` here inside the callback because it's asynchronous!
+        alert('Successfully added to favorites!');
+    });
+}
 
 function App() {
-  return (
-    <>
-      <Button size="icon" variant="outline" className="sm:hidden">
-        Like
-      </Button>
-    </>
-  )
+    return (
+        <>
+            <Button variant="outline" onClick={onClick}>
+                Like
+            </Button>
+        </>
+    )
 }
 
 export default App
