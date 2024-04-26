424d6fea80d604baceda217527c4c0a9f5d44db7
wuz1mu <kingaal93@gmail.com>
2024-04-26 22:51:04 +0300

    implement mock model workflow

--- backend/app/worker/worker.py
+++ backend/app/worker/worker.py
@@ -3,7 +3,9 @@ import os
 import time
 
 from celery import Celery
-from app.db import prisma
+import json
+from celery import chain, group, chord
+from collections import namedtuple
 
 
 celery = Celery(__name__)
@@ -11,12 +13,51 @@ celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
 
-@celery.task(name="create_task")
-def create_task(request_id, task_time, msg):
-    time.sleep(int(task_time) * 10)
-    prisma.prisma_client.modelresponse.create({
-        "request_id": request_id,
-        "started_at": datetime.datetime.now()
-    })
-    print(msg)
+@celery.task(name="create_html_workflow")
+def plan_for_url(url: str):
+    chain(url_extraction.s(url), model_invocation.s()).delay()
     return True
+
+
+@celery.task(name="create_html_workflow")
+def plan_for_url(url: str):
+    chain(url_extraction.s(url), model_invocation.s()).delay()
+    return True
+
+
+@celery.task(name="create_pdf_workflow")
+def plan_for_text(text: str):
+    chain(text_extraction.s(text), model_invocation.s()).delay()
+    return True
+
+
+@celery.task(name="pdf_extraction")
+def pdf_extraction(file_path: str) -> str:
+    return json.dumps({
+        "title": f"some_vacancy_from_pdf: {file_path}"
+    })
+
+
+@celery.task(name="url_extraction")
+def url_extraction(url: str) -> str:
+    return json.dumps({
+        "title": f"some_vacancy_from_url: {url}"
+    })
+
+
+@celery.task(name="text_extraction")
+def text_extraction(text: str) -> str:
+    return json.dumps({
+        "title": f"some_vacancy_from_text: {text}"
+    })
+
+
+@celery.task(name="model_invocation")
+def model_invocation(vacancy_ser: str) -> str:
+    vacancy = json.loads(vacancy_ser, object_hook=custom_vacancy_decoder)
+    return f"model call result for vacancy {vacancy.title}"
+
+
+def custom_vacancy_decoder(vacancy_dict):
+    return namedtuple('X', vacancy_dict.keys())(*vacancy_dict.values())
+
