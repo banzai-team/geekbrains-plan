d4f40f97858bc4c584190f85f243ceedf28e7038
Artem Astapenko <jamakase54@gmail.com>
2024-04-27 11:54:52 +0300

    Add ml call

--- backend/app/worker/worker.py
+++ backend/app/worker/worker.py
@@ -1,22 +1,18 @@
-import datetime
-import os
-import time
-
-from celery import Celery
 import json
-from celery import chain, group, chord
+import os
 from collections import namedtuple
 
+import requests
+from bs4 import BeautifulSoup
+from celery import Celery
+from celery import chain
+from langchain_community.document_loaders import AsyncChromiumLoader
+
+from app.config.config import ML_SERVICE_URL
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
-
-
-@celery.task(name="create_html_workflow")
-def plan_for_url(url: str):
-    chain(url_extraction.s(url), model_invocation.s()).delay()
-    return True
 
 
 @celery.task(name="create_html_workflow")
@@ -39,10 +35,9 @@ def pdf_extraction(file_path: str) -> str:
 
 
 @celery.task(name="url_extraction")
-def url_extraction(url: str) -> str:
-    return json.dumps({
-        "title": f"some_vacancy_from_url: {url}"
-    })
+def url_extraction(url: str):
+    extracted_text = parse_html(url)
+    return extracted_text
 
 
 @celery.task(name="text_extraction")
@@ -53,11 +48,36 @@ def text_extraction(text: str) -> str:
 
 
 @celery.task(name="model_invocation")
-def model_invocation(vacancy_ser: str) -> str:
-    vacancy = json.loads(vacancy_ser, object_hook=custom_vacancy_decoder)
-    return f"model call result for vacancy {vacancy.title}"
+def model_invocation(vacancy_ser: str) -> int:
+    response = requests.post(ML_SERVICE_URL, json={'text': vacancy_ser})
+
+    course = response.json().result
+    print(course)
+    return f"model call result for vacancy {course[0]}"
 
 
 def custom_vacancy_decoder(vacancy_dict):
     return namedtuple('X', vacancy_dict.keys())(*vacancy_dict.values())
 
+
+# Load HTML
+def parse_html(html: str) -> str:
+    loader = AsyncChromiumLoader([html])
+    html = loader.load()
+
+    soup = BeautifulSoup(html[0].page_content, 'html.parser')
+
+    # Remove all tags except 'text' and 'div'
+    for tag in soup.find_all(True):
+        if tag.name not in ['p', 'ul', 'li', 'div'] + [f"h{i}" for i in range(1, 6)]:
+            tag.unwrap()
+
+    for tag in soup.find_all(True):
+        tag.attrs = {}
+        if not tag.get_text(strip=True):
+            tag.decompose()
+
+    sample_text = str(soup)
+    sample_text = '\n'.join(line.strip() for line in sample_text.split('\n') if len(line.strip()) > 0)
+
+    return sample_text
